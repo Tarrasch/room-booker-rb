15ab0b56e2a5d0bba4a0ea4b75ed7731eb582f90
Arash Rouhani <miffoljud@gmail.com>
2012-01-16 16:02:35 +0100

    Add configuation possibilities

--- worker.rb
+++ worker.rb
@@ -2,14 +2,20 @@ require "rest-client"
 require "nokogiri"
 require "yaml"
 
-data = {    
+username = `echo $USERNAME2`.strip
+password = `echo $PASSWORD`.strip
+
+data = {
   authServer: "student",
-  username: "oleander",
-  password: "abc123",
+  username: username,
+  password: password,
   fragment: nil,
   x: 0,
   y: 0
 }
-  
+
+p username
+
 data = RestClient.post("https://web.timeedit.se/chalmers_se/db1/b1/", data)
 puts data.cookies.to_yaml
+puts !! data.match(/Arash Rouhani/)
