512403e481678ee2bd826e92fa858bf34ee7a369
Linus Oleander <linus@oleander.nu>
2012-01-20 01:43:39 +0100

    Minor cleanup

--- worker.rb
+++ worker.rb
@@ -13,9 +13,14 @@ data = {
   y: 0
 }
 
+from    = 12 # 12:00
+to      = 15 # 15:00
+date    = "20120121"
 cookies = {}
-data = RestClient.post("https://web.timeedit.se/chalmers_se/db1/b1/", data) 
-  { |response, request, result, &block| cookies = response.cookies}
+
+data = RestClient.post("https://web.timeedit.se/chalmers_se/db1/b1/", data) { |response, request, result, &block| 
+  cookies = response.cookies
+}
 
 rooms = %w{
   https://web.timeedit.se/chalmers_se/db1/timeedit/p/b1/objects.html?
@@ -29,19 +34,16 @@ rooms = %w{
   grp=5&
   objects=160177.184,203433.185&
   types=186&
-  dates=20120119&
-  starttime=23:0&
-  endtime=24:0
-}.join
+  dates=%s&
+  starttime=%s:0&
+  endtime=%s:0
+}.join % [date, from, to]
 
 # A list of rooms
 doc     = Nokogiri::HTML(RestClient.get(rooms, cookies: cookies))
 rooms   = doc.css(".infoboxtitle").map{|r| r.text} # A list of rooms ["5013", "..."]
 room    = rooms.sample # Selecting random room "5013"
 room_id = doc.at_css("[data-name='#{room}']").attr("data-id")
-from    = 12 # 12:00
-to      = 15 # 15:00
-date    = "20120121"
 
 url = %w{
   https://web.timeedit.se/chalmers_se/db1/timeedit/p/b1/r.html?
@@ -52,13 +54,13 @@ url = %w{
   cn=0&
   id=-1&
   dates=%s&
-  startTime=%s:0&
-  endTime=%s:0&
+  startTime=%s&
+  endTime=%s&
   o=%s&
   o=%s&
-  o=%s,%s&
+  o=%s&
   nocache=3
-}.join % [date, from, to, CGI.escape("160177.184,pr101"), CGI.escape("203433.185,ks91084"), room_id, room]
+}.join % [date, CGI.escape("#{from}:0"), CGI.escape("#{to}:0"), CGI.escape("160177.184,pr101"), CGI.escape("203433.185,ks91084"), CGI.escape("#{room_id},#{room}")]
 
 post_data = {
   dates: date,
@@ -68,7 +70,6 @@ post_data = {
   id: -1,
   kind: "reserve",
   startTime: "#{from}:00",
-  uri: url
 }.each_pair.map{|index, value| "#{index}=#{CGI.escape(value.to_s)}"}.join("&") + "&o=160177.184&o=203433.185&o=#{room_id}"
 
 # TODO: Make it work
