1ce8ebe6a95d3ca29f00dba38f9f9ae533e8e39d
Linus Oleander <linus@oleander.nu>
2012-01-23 02:06:54 +0100

    Internal errors should be reported to user

--- config/jobs.rb
+++ config/jobs.rb
@@ -7,18 +7,37 @@ include Stalker
 
 Stalker.censor_params(:password, :username)
 
+error do |e, job, args|
+  sf = SecureFaye::Connect.new.
+    token("65E16044301D624A9F8741430755B5112AE4562F").
+    server("http://0.0.0.0:9999/faye").
+    channel("/reserve/" + args["uuid"])
+    
+  sf.message({
+    notification: "Error on server side: #{e.message}",
+    type: "message"
+  }.to_json).send!
+end
+
 job "timeedit" do |args|
   sf = SecureFaye::Connect.new.
     token("65E16044301D624A9F8741430755B5112AE4562F").
     server("http://0.0.0.0:9999/faye").
     channel("/reserve/" + args["uuid"])
   
-  rb = RoomBooker.new({
-    from: args["from"],
-    to: args["to"],
-    password: args["password"],
-    username: args["username"]
-  })
+  begin
+    rb = RoomBooker.new({
+      from: args["from"],
+      to: args["to"],
+      password: args["password"],
+      username: args["username"]
+    })
+  rescue RuntimeError
+    sf.message({
+      notification: $!.message,
+      type: "message"
+    }.to_json).send!; next
+  end
   
   unless rb.valid_credentials?
     sf.message({
@@ -29,16 +48,17 @@ job "timeedit" do |args|
   
   args["days"].each do |day|
     rb.date = Time.at(day.to_i)
+    
     if room = rb.rooms.select{ |room| room.split("").first == args["floor"].to_s }.first
       begin
         rb.book!(room)
       rescue
-        message = $!.message || "Something went wrong"
+        message ||= $!.message || "Something went wrong"
       ensure
         message ||= "valid"
       end
     else
-      message = "no_room"
+      message ||= "no_room"
     end
     
     hash = {
